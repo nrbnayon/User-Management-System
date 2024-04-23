b233d734783c26508872b971f67a88ab41268066
nrbnayon <nrbnayon@gmail.com>
2024-04-23 22:36:07 +0600

    Mongodb setup

--- Client/src/Components/Users/Users.jsx
+++ Client/src/Components/Users/Users.jsx
@@ -1,18 +1,168 @@
-import { useState } from "react";
-import { useLoaderData } from "react-router-dom";
-
 const Users = () => {
-  const [user, setUser] = useState([]);
-  const users = useLoaderData();
-  console.log(users);
+  const handleAddUser = async (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
+
+    try {
+      const response = await fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to create user");
+      }
+
+      const newUser = await response.json();
+      console.log("New User", newUser);
+    } catch (error) {
+      console.error("Error creating user:", error);
+    }
+  };
+
   return (
     <div>
       <h3 className="text-3xl text-center font-bold">User Management System</h3>
-      <h3 className="text-xl text-center font-bold">
-        Total User {users.length}
-      </h3>
+      <h3 className="text-xl text-center font-bold"></h3>
+      <div>
+        <form onSubmit={handleAddUser} className="max-w-md mx-auto">
+          <div className="mb-4">
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              // onChange={handleInputChange}
+              className="border border-gray-300 rounded-md px-4 py-2 w-full focus:outline-none focus:border-blue-500"
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              // onChange={handleInputChange}
+              className="border border-gray-300 rounded-md px-4 py-2 w-full focus:outline-none focus:border-blue-500"
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 w-full hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
 
 export default Users;
+
+// import { useState, useEffect } from "react";
+// import { useLoaderData } from "react-router-dom";
+
+// const Users = () => {
+//   const initialFormState = { name: "", email: "" };
+//   const usersData = useLoaderData();
+//   const [users, setUsers] = useState(usersData);
+//   const [userForm, setUserForm] = useState(initialFormState);
+
+//   useEffect(() => {
+//     fetchUsers();
+//   }, []);
+
+//   const fetchUsers = async () => {
+//     try {
+//       const response = await fetch("http://localhost:3000/users");
+//       if (!response.ok) {
+//         throw new Error("Failed to fetch users");
+//       }
+//       const data = await response.json();
+//       setUsers(data);
+//     } catch (error) {
+//       console.error("Error fetching users:", error);
+//     }
+//   };
+
+//   const handleInputChange = (e) => {
+//     const { name, value } = e.target;
+//     setUserForm({ ...userForm, [name]: value });
+//   };
+
+//   const handleAddUser = async (e) => {
+//     e.preventDefault();
+
+//     try {
+//       const response = await fetch("http://localhost:3000/users", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(userForm),
+//       });
+//       if (!response.ok) {
+//         throw new Error("Failed to create user");
+//       }
+
+//       const newUser = await response.json();
+//       setUsers([...users, newUser]);
+//       setUserForm(initialFormState);
+//     } catch (error) {
+//       console.error("Error creating user:", error);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h3 className="text-3xl text-center font-bold">User Management System</h3>
+//       <h3 className="text-xl text-center font-bold">
+//         Total User {users.length}
+//       </h3>
+//       <div>
+//         <form onSubmit={handleAddUser} className="max-w-md mx-auto">
+//           <div className="mb-4">
+//             <input
+//               type="text"
+//               placeholder="Name"
+//               name="name"
+//               value={userForm.name}
+//               onChange={handleInputChange}
+//               className="border border-gray-300 rounded-md px-4 py-2 w-full focus:outline-none focus:border-blue-500"
+//             />
+//           </div>
+//           <div className="mb-4">
+//             <input
+//               type="text"
+//               name="email"
+//               placeholder="Email"
+//               value={userForm.email}
+//               onChange={handleInputChange}
+//               className="border border-gray-300 rounded-md px-4 py-2 w-full focus:outline-none focus:border-blue-500"
+//             />
+//           </div>
+//           <button
+//             type="submit"
+//             className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline"
+//           >
+//             Submit
+//           </button>
+//         </form>
+//       </div>
+//       <div className="text-center">
+//         {users.map((user) => (
+//           <li key={user.id}>{user.name}</li>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Users;
--- server/index.js
+++ server/index.js
@@ -1,80 +1,191 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const path = require("path");
+const { MongoClient } = require("mongodb");
 
-// Load environment variables from .env file
 dotenv.config();
-
-const users = [
-  {
-    id: 1,
-    name: "John Doe",
-    email: "john@example.com",
-    age: 30,
-    address: "123 Main St, Anytown, USA",
-  },
-  {
-    id: 2,
-    name: "Jane Smith",
-    email: "jane@example.com",
-    age: 25,
-    address: "456 Elm St, Othertown, USA",
-  },
-  {
-    id: 3,
-    name: "Alice Johnson",
-    email: "alice@example.com",
-    age: 35,
-    address: "789 Oak St, Anycity, USA",
-  },
-  {
-    id: 4,
-    name: "Bob Brown",
-    email: "bob@example.com",
-    age: 40,
-    address: "101 Pine St, Someville, USA",
-  },
-  {
-    id: 5,
-    name: "Emily Davis",
-    email: "emily@example.com",
-    age: 28,
-    address: "202 Maple St, Newcity, USA",
-  },
-];
-
-const villas = require("./luxury.json");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware
+app.use(express.json());
 app.use(cors());
 
-// Routes
-app.get("/", (req, res) => {
-  res.send("User Management System is running");
-});
+const uri = process.env.MONGODB;
+const client = new MongoClient(uri);
 
-app.get("/data", (req, res) => {
-  res.send("Data Coming");
-});
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB!");
 
-app.get("/users", (req, res) => {
-  res.send(users);
-});
+    const database = client.db("userDB").collection("users");
 
-app.get("/villas", (req, res) => {
-  res.send(villas);
-});
+    app.get("/", (req, res) => {
+      res.send("User Management System is running");
+    });
 
-app.get("/villas/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const villa = villas.find((villa) => villa.id === id) || {};
-  res.send(villa);
-});
+    app.get("/users", async (req, res) => {
+      try {
+        const user = await database.find({}).toArray();
+        res.json(user);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
 
-// Server Listening
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+    app.post("/users", async (req, res) => {
+      try {
+        const newUser = req.body;
+        console.log(newUser);
+        const result = await database.insertOne(newUser);
+        if (result.insertedCount === 1) {
+          res.status(201).json(newUser);
+        } else {
+          console.error(
+            "Error creating user: No data returned from MongoDB insert operation."
+          );
+          res.status(500).json({ error: "Internal Server Error" });
+        }
+      } catch (error) {
+        console.error("Error creating user:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } finally {
+    // Ensure to close the connection when done
+    // await client.close();
+  }
+}
+
+run().catch(console.error);
+
+// const express = require("express");
+// const cors = require("cors");
+// const dotenv = require("dotenv");
+// const path = require("path");
+
+// // Load environment variables from .env file
+// dotenv.config();
+
+// const users = [
+//   {
+//     id: 1,
+//     name: "John Doe",
+//     email: "john@example.com",
+//     age: 30,
+//     address: "123 Main St, Anytown, USA",
+//   },
+//   {
+//     id: 2,
+//     name: "Jane Smith",
+//     email: "jane@example.com",
+//     age: 25,
+//     address: "456 Elm St, Othertown, USA",
+//   },
+//   {
+//     id: 3,
+//     name: "Alice Johnson",
+//     email: "alice@example.com",
+//     age: 35,
+//     address: "789 Oak St, Anycity, USA",
+//   },
+//   {
+//     id: 4,
+//     name: "Bob Brown",
+//     email: "bob@example.com",
+//     age: 40,
+//     address: "101 Pine St, Someville, USA",
+//   },
+//   {
+//     id: 5,
+//     name: "Emily Davis",
+//     email: "emily@example.com",
+//     age: 28,
+//     address: "202 Maple St, Newcity, USA",
+//   },
+// ];
+
+// const villas = require("./luxury.json");
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// app.use(cors());
+// app.use(express.json());
+
+// const { MongoClient, ServerApiVersion } = require("mongodb");
+// const uri = process.env.MONGODB;
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
+
+// async function run() {
+//   try {
+//     await client.connect();
+//     const database = client.db("userDB").collection("users");
+//     app.post("/users", async (req, res) => {
+//       try {
+//         const database = client.db("userDB").collection("users");
+//         const newUser = req.body;
+//         const result = await database.insertOne(newUser);
+//         res.status(201).json(result.ops[0]); // Send the inserted user data as response
+//       } catch (error) {
+//         console.error("Error creating user:", error);
+//         res.status(500).json({ error: "Internal Server Error" }); // Send an error response
+//       }
+//     });
+
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("You successfully connected to MongoDB!");
+//   } finally {
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+// // Routes
+// app.get("/", (req, res) => {
+//   res.send("User Management System is running");
+// });
+
+// app.get("/data", (req, res) => {
+//   res.send("Data Coming");
+// });
+
+// app.get("/users", (req, res) => {
+//   res.send(users);
+// });
+
+// // app.post("/users", (req, res) => {
+// //   console.log("Api Hitting");
+// //   console.log(req.body);
+
+// //   const newUser = req.body;
+// //   newUser.id = users.length + 1;
+// //   users.push(newUser);
+
+// //   res.status(201).json(newUser);
+// // });
+
+// app.get("/villas", (req, res) => {
+//   res.send(villas);
+// });
+
+// app.get("/villas/:id", (req, res) => {
+//   const id = parseInt(req.params.id);
+//   const villa = villas.find((villa) => villa.id === id) || {};
+//   res.send(villa);
+// });
+
+// // Server Listening
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
